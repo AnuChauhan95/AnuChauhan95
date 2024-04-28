55181a1f8b90ac8ecd51fc3270f9eb0bfc823762
Anupam Chauhan <145430383+AnuChauhan95@users.noreply.github.com>
2024-04-28 17:07:34 +0530

    Add files via upload

---
+++ AllModels/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllModels.Models
+{
+    public class Student
+    {
+        public Guid StudId { get; set; }
+        public string StudName { get; set; }
+        public string Address { get; set; }
+        public string MyProperty { get; set; }
+       
+    }
+}
